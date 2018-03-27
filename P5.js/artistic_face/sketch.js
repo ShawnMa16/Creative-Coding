fdb290ade108dae93075d6d49521fef072232d9d
Shawn Ma <emailme.mx@gmail.com>
2018-03-27 16:27:20 -0400

    first try of using BRFv4

--- P5.js/artistic_face/sketch.js
+++ P5.js/artistic_face/sketch.js
@@ -1,7 +1,24 @@
+let face = [];
+
+
 function setup() {
   // put setup code here
+
+  createCanvas(540, 720);
+
 }
 
 function draw() {
   // put drawing code here
+}
+
+function drawFace() {
+
+  if (face.state === brfv4.BRFState.FACE_TRACKING_START || face.state === brfv4.BRFState.FACE_TRACKING) {
+   face.points.forEach(function(point) {
+
+    console.log(point);
+   }
+  );
+  }
 }
