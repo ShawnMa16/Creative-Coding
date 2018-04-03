c670f0daed0d116b2af0198a76f303254bacaabc
Shawn Ma <emailme.mx@gmail.com>
2018-04-03 14:15:04 -0400

    added https server

--- P5.js/water_ripple_mirror/webserver.js
+++ P5.js/water_ripple_mirror/webserver.js
@@ -1,25 +1,35 @@
-var express = require('express');
+let https = require('https');
+let fs = require('fs'); // Using the filesystem module
 
-var app = express();
+let credentials = {
+  key: fs.readFileSync('/etc/letsencrypt/live/xiaoma.space/privkey.pem'),
+  cert: fs.readFileSync('/etc/letsencrypt/live/xiaoma.space/cert.pem')
+};
+let express = require('express');
+
+let app = express();
 
 app.use(express.static('public'));
 app.use(express.static('js/scripts'));
 app.use(express.static('js/libs'));
 
-app.set('view engine', 'ejs');
-
 app.get('/water_ripple', function (req, res) {
 
   /* Alternatively you could loop through the records with a "for"
-  	for (var i = 0; i < saved.length; i++) {
+  	for (let i = 0; i < saved.length; i++) {
 	  	console.log(saved[i]);
 	}
   */
+  //  res.sendFile('./public/index.html');
 });
+
+// app.listen(80, function () {
+//   console.log('Example app listening on port 80!')
 // });
 
-app.listen(3080, function () {
-  console.log('Example app listening on port 3080!')
-});
+let httpsServer = https.createServer(credentials, app);
+
+// Default HTTPS Port
+httpsServer.listen(80);
 
 // module.exports = app;
